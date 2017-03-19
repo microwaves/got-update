0dc3e712ff0a89b4c1f66a1bc56f5e227f3b4dca
microwaves <stephanozanzin@gmail.com>
2017-03-19 02:25:07 +0000

    Cleaning the house. :nail_care:

--- got_update.rb
+++ got_update.rb
@@ -3,17 +3,17 @@ require 'open-uri'
 require 'mail'
 
 class GotUpdate
-	attr_reader :mail_options, :mail_from, :recipient, :website, :target_content, :current_content
+  attr_reader :mail_options, :mail_from, :recipient, :website, :target_content, :current_content
 
-	def initialize(recipient, website, target_content)
-		@mail_options = {
-			:address => 'SMTP_SERVER_HOST',
-			:port => SMTP_SERVER_PORT,
-			:user_name => 'EMAIL_USERNAME',
-			:password => 'EMAIL_PASSWORD',
-			:authentication => :plain,
-			:enable_starttls_auto => true
-		}
+  def initialize(recipient, website, target_content)
+    @mail_options = {
+      :address => 'SMTP_SERVER_HOST',
+      :port => SMTP_SERVER_PORT,
+      :user_name => 'EMAIL_USERNAME',
+      :password => 'EMAIL_PASSWORD',
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
     setup_mail
 
     @mail_from = 'MAIL_FROM_ADDRESS'
@@ -21,52 +21,52 @@ class GotUpdate
     @website = website
     @target_content = target_content
     @current_content = retrieve_content
-	end
+  end
 
-	def listen!
-		while true
-			check_for_updates
-			sleep 2
-		end
-	end
+  def listen!
+    while true
+      check_for_updates
+      sleep 2
+    end
+  end
 
-	private
+  private
 
   def setup_mail
-		Mail.defaults do
-			delivery_method :smtp, @mail_options
-		end
+    Mail.defaults do
+      delivery_method :smtp, @mail_options
+    end
   end
 
   def retrieve_content
-		Nokogiri::HTML(
+    Nokogiri::HTML(
       open(@website)
     ).css(@target_content).to_s
   end
 
-	def check_for_updates
+  def check_for_updates
     @new_content = retrieve_content
 
-		if @new_content != @current_content
+    if @new_content != @current_content
       puts "#####################################"
       puts "#         WEBSITE UPDATED!          #"
       puts "#####################################"
 
-			send_email
-			abort
+      send_email
+      abort
     else
       puts "No updates yet..."
     end
-	end
+  end
 
-	def send_email
-		Mail.deliver do
-			to @recipient
-			from @mail_from 
-			subject "#{@website} got some updates!"
-			body "Access #{@website} and check it with your own eyes."
-		end
-	end
+  def send_email
+    Mail.deliver do
+      to @recipient
+      from @mail_from
+      subject "#{@website} got some updates!"
+      body "Access #{@website} and check it with your own eyes."
+    end
+  end
 end
 
 #crawler = GotUpdate.new('foobar@xyz.net', 'http://foobar.foo/news', 'div #container ul')
